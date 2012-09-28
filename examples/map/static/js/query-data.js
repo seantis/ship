b82fee7c1b85ec47fc9031c3a3707c40d158bf75
Denis Krienbühl <denis.krienbuehl@gmail.com>
2012-09-28 20:26:56 +0200

    Adds query update function

--- examples/map/static/js/query-data.js
+++ examples/map/static/js/query-data.js
@@ -1,14 +1,44 @@
+var query_cache = {};
+
 var update_premiums = function() {
     var year = $('input[name="yearRadio"]:checked').val();
     var age = $('input[name="ageRadio"]:checked').val();
     var franchise = $('#deductibleLabel').text();
     
-    query_premiums(year, age, franchise, function(data) {
-        console.log(data);
-    });
+    query_premiums(year, age, franchise, handle_update);
 };
 
 var query_premiums = function(year, age, franchise, callback) {
     var url = '/query?age=' + age + '&year=' + year + '&franchise=' + franchise;
-    jQuery.getJSON(url, callback);
+
+    if (url in query_cache) {
+        callback(query_cache[url]);
+        return;
+    };
+
+    var update_cache = function(data) {
+        query_cache[url] = data;
+        callback(data);
+    };
+
+    jQuery.getJSON(url, update_cache);
+};
+
+var handle_update = function(prices) {
+    var min = 10000000, max = 0;
+    for (var i = 0; i < prices.length; i++) {
+        if (prices[i].premium < min) {
+            min = prices[i].premium;
+        }
+
+        if (prices[i].premium > max) {
+            max = prices[i].premium;
+        }
+    }
+
+    var quantize = d3.scale.quantile().domain([min, max]).range(d3.range(9));
+    for (i = 0; i < prices.length; i++) {
+        var id = '#canton-' + prices[i].canton.toLowerCase();
+        $(id).attr('class', 'q' + quantize(prices[i].premium) + '-9');
+    }
 };
