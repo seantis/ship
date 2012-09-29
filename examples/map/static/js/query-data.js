6a5d4e5c9287292e87b1c75a42a03877e5e14490
Denis Krienbühl <denis.krienbuehl@gmail.com>
2012-09-29 13:30:19 +0200

    Adds loading status

--- examples/map/static/js/query-data.js
+++ examples/map/static/js/query-data.js
@@ -1,6 +1,39 @@
 var query_cache = {};
 
+var transforms = ['-webkit-transform', '-moz-transform', '-ms-transform', '-o-transform']
+
+var rotate_logo = function() {
+    var rotation = 0, interval = 0;
+    var logo = $('#title img');
+
+    if (logo.data('rotating')) {
+        return;
+    }
+
+    logo.data('rotating', true);
+    var rotate = function() {
+        rotation += 10;
+        for (var i=0; i < transforms.length; i++) {
+            logo.css(transforms[i], "rotate(" + rotation + "deg)");
+        }
+
+        if (rotation == 350) {
+            clearInterval(interval);
+
+            for (i=0; i < transforms.length; i++) {
+                logo.css(transforms[i], "");
+            }    
+
+            logo.data('rotating', false);
+        }
+    }
+    
+    interval = setInterval(rotate, 10);
+}
+
 var update_premiums = function(callback) {
+    rotate_logo();
+
     var year = $('input[name="yearRadio"]:checked').val();
     var age = $('input[name="ageRadio"]:checked').val();
     var franchise = $('#deductibleLabel').text();
